e195d4edbc50fad90d3c5338e58b46f43b8ca17e
Erish <erish968@gmail.com>
2026-05-11 20:49:17 +0545

    first push to repository

---
+++ src/main/java/com/example/myFirstProject/myClass.java
@@ -0,0 +1,13 @@
+package com.example.myFirstProject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class myClass {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello";
+    }
+
+}
