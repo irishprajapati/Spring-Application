ca022b02a8017e24a854bfe738aa145a9118ecb2
Erish <erish968@gmail.com>
2026-05-12 19:35:28 +0545

    Feat: Dependency injection in same package

--- src/main/java/com/example/myFirstProject/myClass.java
+++ src/main/java/com/example/myFirstProject/myClass.java
@@ -3,11 +3,10 @@ package com.example.myFirstProject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController //specialized version of component + something else
 public class myClass {
     @GetMapping("/hello")
     public String sayHello(){
         return "Hello";
     }
-
 }
