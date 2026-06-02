7da50e7a566193b36a1746b0400a111a9582a64d
Erish <erish968@gmail.com>
2026-06-02 20:18:30 +0545

    Feat: add the Service with interface implementation

--- src/main/java/com/example/myFirstProject/service/JournalEntryService.java
+++ src/main/java/com/example/myFirstProject/service/JournalEntryService.java
@@ -2,8 +2,12 @@ package com.example.myFirstProject.service;
 
 import com.example.myFirstProject.entity.JournalEntry;
 import com.example.myFirstProject.repository.JournalEntryRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class JournalEntryService {
@@ -17,5 +21,14 @@ public class JournalEntryService {
         System.out.println("SAVED: " + saved);
 
         return saved;
+    }
+    public List<JournalEntry> getAll(){
+        return journalEntryRepository.findAll();
+    }
+    public Optional<JournalEntry> findById(ObjectId id){
+        return journalEntryRepository.findById(id);
+    }
+    public void deleteById(ObjectId id){
+        journalEntryRepository.deleteById(id);
     }
 }
