b2476efea4efc318439483cc732622521a7c32ec
Erish <erish968@gmail.com>
2026-06-12 16:40:58 +0545

    feat: add the authentication through security context holder

--- src/main/java/com/example/myFirstProject/controller/UserController.java
+++ src/main/java/com/example/myFirstProject/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.example.myFirstProject.controller;
 
 import com.example.myFirstProject.entity.User;
+import com.example.myFirstProject.repository.UserRepository;
 import com.example.myFirstProject.service.UserService;
 import org.bson.types.ObjectId;
+import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,19 +20,13 @@ import java.util.Optional;
 public class UserController {
     @Autowired
     private UserService userService;
-    @GetMapping
-    public List<User> getAllUser(){
-        return userService.getAllUser();
-    }
-    @PostMapping
-    public ResponseEntity<?> createUser(@RequestBody User user){
-        try{
-            userService.saveUser(user);
-            return new ResponseEntity<>(user, HttpStatus.CREATED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        }
-    }
+    @Autowired
+    private UserRepository userRepository;
+//    @GetMapping
+//    public List<User> getAllUser(){
+//        return userService.getAllUser();
+//    }
+
     @GetMapping("/user/{myId}")
     public ResponseEntity<?> getUserById(@PathVariable ObjectId id){
         Optional<User> user = userService.findUserById(id);
@@ -37,17 +35,23 @@ public class UserController {
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-    @DeleteMapping("/user/{myId}")
-    public boolean deleteUserById(@PathVariable ObjectId id){
+    @DeleteMapping
+    public boolean deleteUserByIdAndUserName(@PathVariable ObjectId id){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        userRepository.deleteByUserName(authentication.getName());
         userService.deleteById(id);
         return true;
     }
-    @PutMapping("/{userName}")
-    public ResponseEntity<?> updateUser(@RequestBody User user, @PathVariable String userName) {
+    @PutMapping
+    public ResponseEntity<?> updateUser(@RequestBody User user) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String userName = authentication.getName();
         User userInDb = userService.findByUserName(userName);
         if(userInDb != null){
             userInDb.setUserName(user.getUserName());
-            userInDb.setPassword(user.getPassword());
+            if(user.getPassword() != null && !user.getPassword().isBlank()){
+                userInDb.setPassword(user.getPassword());
+            }
             userService.saveUser(userInDb);
         }
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
