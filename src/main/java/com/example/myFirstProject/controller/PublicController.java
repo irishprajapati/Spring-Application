a2fc1d5d0b3aced5ec201303fdb9c7bd8261f63c
Erish <erish968@gmail.com>
2026-06-12 16:38:57 +0545

    feat: update the health check to public controller

--- src/main/java/com/example/myFirstProject/controller/PublicController.java
+++ src/main/java/com/example/myFirstProject/controller/PublicController.java
@@ -1,17 +1,35 @@
 package com.example.myFirstProject.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.myFirstProject.entity.User;
+import com.example.myFirstProject.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class HealthCheck {
+@RequestMapping("/public")
+public class PublicController {
+    @Autowired
+    private UserService userService;
     @GetMapping("/health")
-    public String ValidateHealth(){
+    public String ValidateHealth() {
         // this is to check whether the API endpoints are working fine or not in browser
         return "Okay";
     }
+
     @GetMapping("/ping")
-    public String validatePing(){
+    public String validatePing() {
         return "Pong";
+    }
+
+    @PostMapping("/create-user")
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        try {
+            userService.saveUser(user);
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 }
